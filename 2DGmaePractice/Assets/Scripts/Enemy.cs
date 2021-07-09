3fff7d3013880118d9ad43adf39efa83239ad157
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2021-07-09 23:44:05 +0900

    7/9

---
+++ 2DGmaePractice/Assets/Scripts/Enemy.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject
+{
+    // Start is called before the first frame update
+
+    public int playerDamage;
+
+    private Animator animator;
+    private Transform target; //플레이어 위치를 저장, 적이 어디로 향할지 알려줌
+    private bool skipMove; //적이 움직이게끔 하는 변수
+
+    protected override void Start()
+    {
+        GameManager1.instance.AddEnemyToList(this);
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;//플레이어 위치를 받아옴
+        base.Start();
+    }
+
+    protected override void AttemptMove <T> (int xDir, int yDir)
+    {
+        if (skipMove)//매번 턴이 돌아올 때만 움직임
+        {
+            skipMove = false;
+            return;
+        }
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        skipMove = true;
+    }
+
+    public void MoveEnemy()//적들을 움직이려 할때 게임 매니저에 의해 호출될 함수
+    {
+        int xDir = 0;
+        int yDir = 0;
+
+        if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)//대충 x 좌표가 같은지 체크, 적과 플레이어가 같은 열
+            yDir = target.position.y > transform.position.y ? 1 : -1; //플레이어의 y 좌표가 더 크다면 적을 y++
+        else
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+
+        AttemptMove<Player>(xDir, yDir);
+    }
+
+    protected override void OnCantMove <T> (T component)
+    {
+        Player hitPlayer = component as Player;
+
+        animator.SetTrigger("enemyAttack");
+
+        hitPlayer.LoseFood(playerDamage);//플레이어의 losefood  를 호출하고 적의 공격으로 잃어버릴 음식점수가 될 playerDamage 입력
+
+    }
+}
--- 2DGmaePractice/Assets/Scripts/GameManager1.cs
+++ 2DGmaePractice/Assets/Scripts/GameManager1.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class GameManager1 : MonoBehaviour
 {
+    public float turnDelay = .1f;
     public BoardManager boardScript;
     public static GameManager1 instance = null; // 싱글턴 (두개의 게임매니저가 발생하지 않도록) 을 위한 변수
     public int playerFoodPoints = 100;
     [HideInInspector] public bool playersTurn = true; // 퍼블릭이지만 에디터에서는 숨겨지게 하기 위한 [HideInInspector]
 
     private int level = 3; //f레벨 2부터 적등장
+    private List<Enemy> enemies;//적들의 위치 계속 추적, 명령
+    private bool enemiesMoving;
 
     // Start is called before the first frame update
     void Awake()
@@ -20,12 +23,14 @@ public class GameManager1 : MonoBehaviour
             Destroy(gameObject); // 두개의 게임매니저면 파괴
 
         DontDestroyOnLoad(gameObject); // 점수를 계산하기위해 신이 넘어갈때 게임오브젝트를 제거하지 않도록 함
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
 
     void InitGame()
     {
+        enemies.Clear();// 이전 레벨 적들 정리
         boardScript.SetupScene(level); // 보드 스크립트에 구성하는 씬이 몇 레벨인지 알려줄 수 있다.
     }
 
@@ -34,9 +39,38 @@ public class GameManager1 : MonoBehaviour
         enabled = false;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        if (playersTurn || enemiesMoving)// 플레이어 턴 혹은 적이 이동중이라면 실행 취소
+            return;
 
+        StartCoroutine(MoveEnemies());
     }
+
+    public void AddEnemyToList(Enemy script)//게임메니저가 적들이 움직이도록 명령
+    {
+        enemies.Add(script);
+    }
+    // Update is called once per frame
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if(enemies.Count == 0)// 적의 유무 확인
+        {
+            yield return new WaitForSeconds(turnDelay);//적이 없지만 플레이어는 기다리게
+        }
+
+        for(int i = 0;i<enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime); // 다음 적을 호출하기 전에 movetime 변수를 입력하여 대기
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+
+
 }
